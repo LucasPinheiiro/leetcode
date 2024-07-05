3734f01590b07167e3f126e8dfaedae12c9fc687
LucasPinheiro <apinheiro.lucas@gmail.com>
2024-07-05 16:11:23 -0300

    412 fizz buzz fixed

--- leet_code_exercises/src/exercises_leetcode.java
+++ leet_code_exercises/src/exercises_leetcode.java
@@ -15,7 +15,7 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         //Create
         List<String> result = new ArrayList<>();
-        for (int i = 0; i <= n; i++){
+        for (int i = 1; i <= n; i++){
             if((i%3==0) && (i%5==0)){
                 result.add("Fizz");
             }else if(i%3==0){
