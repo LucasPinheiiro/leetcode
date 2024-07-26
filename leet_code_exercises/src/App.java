46c1c7a832c539a5d81f9fc675feb719a4033eef
LucasPinheiro <apinheiro.lucas@gmail.com>
2024-07-26 13:06:53 -0300

    reducing complexity operation of removeduplicates; two pointers method

--- leet_code_exercises/src/App.java
+++ leet_code_exercises/src/App.java
@@ -3,7 +3,7 @@ import solutions.Solution;
 public class App {
     public static void main(String[] args) throws Exception {
         Solution sol = new Solution();
-        int[] testArray5 = {0, 3, 2, 1};
-        System.out.println(sol.validMountainArray(testArray5));
+        int[] testArray5 = {0, 1, 1, 2, 2, 2, 2, 3};
+        System.out.println(sol.removeDuplicates2(testArray5));
     }
 }
--- leet_code_exercises/src/Solutions/Solution.java
+++ leet_code_exercises/src/Solutions/Solution.java
@@ -315,4 +315,29 @@ public enum State {
         }
         return arr;
     }
+//////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+     * 
+     * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+     *   
+     *   Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+     *   
+     *   Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+     *   Return k.
+     */
+    public int removeDuplicates2(int[] nums) {
+        //edge cases
+        if(nums.length == 0)return 0;
+        //the first element is unique
+        int unique_elements = 1;
+        //check for the following elements
+        for(int i = 1; i<nums.length; i++){
+            //two pointers tacks the data, unique_elements tracks the unique elemets
+            //when a new element is found, it just go to unique position and rewrites that position with the unique value
+            if( !(nums[i-1] == nums[i]) ){
+                nums[unique_elements++] = nums[i];
+            }
+        }
+        return unique_elements;
+    }
 }
